c2b07cff60587e0510596ff107e0e1806387b0b4
Anna Kozlova <annakozlova@Annas-MacBook-Pro.local>
2024-10-14 12:44:58 -0400

    Finished Frontend for first 3 pages

--- src/components/CleaningTaskCard.js
+++ src/components/CleaningTaskCard.js
@@ -1,12 +1,15 @@
+// src/components/CleaningTaskCard.js
+import React from 'react';
+
+
 function CleaningTaskCard({ task, description, price }) {
-    return (
-      <div className="cleaning-card">
-        <h3>{task}</h3>
-        <p>{description}</p>
-        <p>Price: ${price}</p>
-      </div>
-    );
-  }
-  
-  export default CleaningTaskCard;
-  
+  return (
+    <div className="cleaning-card">
+      <h3>{task}</h3>
+      <p>{description}</p>
+      <p>Price: ${price}</p>
+    </div>
+  );
+}
+
+export default CleaningTaskCard;
